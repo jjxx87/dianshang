d6bed1f09a693f2cc116ebca8be18252be0e5201
jjxx87 <2963558591@qq.com>
2026-03-01 20:05:35 +0800

    优化第一版

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.models import Product
 
 bp = Blueprint('main', __name__)
@@ -8,3 +8,15 @@ bp = Blueprint('main', __name__)
 def index():
     products = Product.query.all()
     return render_template('index.html', title='首页', products=products)
+
+@bp.route('/search')
+def search():
+    query = request.args.get('q', '')
+    if query:
+        products = Product.query.filter(
+            (Product.name.contains(query)) | 
+            (Product.description.contains(query))
+        ).all()
+    else:
+        products = []
+    return render_template('index.html', title=f'搜索结果: {query}', products=products, search_query=query)
